a3885607d450eb93e37fb1c837a9f81713e8b36b
Borko <korea60@ab.bg>
2020-12-20 16:30:14 +0200

    play more with vue

--- app.js
+++ app.js
@@ -5,12 +5,22 @@ const app = Vue.createApp({
 			author: 'Borko St',
 			age: 39,
 			showContent: true,
+			message: '',
+			//li form
+			todos: [
+				{text: 'First todo'},
+				{text: 'Second todo'},
+				{text: 'Last todo'}
+			],
+			//message from time of a day
+			hours: new Date().getHours(),
+			path: location.pathname,
 		}
 	}, 
 	methods: {
 		changeTitle() {
 			this.showContent = !this.showContent
-		}
+		},
 	}
 })
 
