6762b07b6aaa715e34aa615004e6f823b2a862e5
Borko <bstoilov81@gmail.com>
2020-12-22 15:28:13 +0200

    Use vue cli

---
+++ modal-project/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <h3>{{ title }}</h3>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      title: 'Some title',
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  background-color: bisque;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h3 {
+  border-bottom: 1px solid #ccc;
+  text-transform: uppercase;
+}
+</style>
